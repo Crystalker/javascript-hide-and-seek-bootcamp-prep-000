7d160104eefb0f7b5d802f78801ca2c898d6b200
Crystalker <crystalsemen@gmail.com>
2018-04-01 00:47:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
-function increseRankBy(n) {
+function increaseRankBy(n) {
   const lis = document.querySelector('#app .ranked-list');
   for(let x = 0; x < lis.length; x++) {
     lis[i].innerHTML = (parseInt(lis[i].innerHTML) + 1).toString()
