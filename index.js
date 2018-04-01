410cdc39b234213f62cdc3b61721c6d2830f1838
Crystalker <crystalsemen@gmail.com>
2018-04-01 00:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nestedTarget() {
 
 function increseRankBy(n) {
   const lis = document.querySelector('#app .ranked-list');
-  for(let x = 0; x < lis.length; lis++) {
-    lis[i].innerHtml = parseInt(lis[i].innerHtml)
+  for(let x = 0; x < lis.length; x++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + 1).toString()
   }
 }
