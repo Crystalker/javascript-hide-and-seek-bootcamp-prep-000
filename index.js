5e92739f4c40866c0f78450ae876bcfa760770bf
Crystalker <crystalsemen@gmail.com>
2018-04-01 01:11:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,5 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  const last = document.querySelector('#grand-node div div div div');
-  return last.innerHTML
+  return document.querySelector('#grand-node div div div div').innerHTML;
 }
