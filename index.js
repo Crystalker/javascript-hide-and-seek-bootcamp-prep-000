bc27a3e3446d32a4ed662864cf2939fa2ae42826
Crystalker <crystalsemen@gmail.com>
2018-04-01 01:42:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list li');
+  for(let x = 0; x < lis.length; x++) {
+    lis[x].innerHTML = (parseInt(lis[x].innerHTML) + n).toString()
+  }
+}
+
+function deepestChild() {
+  //return document.querySelector('#grand-node div div div div');
+  let node = document.querySelector('#grand-node')
+  let nextNode = node.children[0];
+  while(nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  return node;
+}
